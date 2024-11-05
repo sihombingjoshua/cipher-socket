48792f1b04d4347c110157006b4dbbec61e25532
sihombingjoshua <joshuasihombing1joshua@gmail.com>
2024-11-05 14:04:43 +0700

    1.0

---
+++ client cipher.py
@@ -0,0 +1,24 @@
+import socket
+import json
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:
+    a = {'data' : 'PoltekSSN', 'key' : 3}
+    ajson = json.dumps(a)
+    ns.sendto(ajson.encode(), ('127.0.0.1', 7777))
+    data, addr=ns.recvfrom(256)
+    print(data.decode())
+
+    shift = int(input("shift?: "))
+
+    while True:
+        b = {}
+        plaintxt = input("plaintxt: ")
+        if plaintxt == 'exit':
+            break
+        b['data'] = plaintxt
+        b['key'] = shift
+        ajson = json.dumps(b)
+        ns.sendto(ajson.encode(), ('127.0.0.1', 7777))
+        data, addr=ns.recvfrom(256)
+        print("ciphertext:", data.decode())
+
+
---
+++ server cipher.py
@@ -0,0 +1,38 @@
+import socket
+import json
+
+class Caesar:
+    def __init__(self, plain, shift):
+        self.plaintext = plain
+        self.shift = shift
+    def cipher(self):
+        results = ''
+        for char in self.plaintext:
+            if char.isupper():
+                results += chr((ord(char) + self.shift - 65) % 26 + 65)
+            elif char.islower():
+                results += chr((ord(char) + self.shift - 97) % 26 + 97)
+            else:
+                results += char
+        self.ciphertext = results
+        return self.ciphertext
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:
+    ns.bind(('127.0.0.1', 7777))
+    data, addr=ns.recvfrom(256)
+    datadump = json.loads(data)
+    cipher1 = Caesar(datadump['data'], datadump['key'])
+    print("Plaintext : " +cipher1.plaintext)
+    print("Ciphertext : "+cipher1.cipher())
+    ns.sendto(cipher1.cipher().encode(), addr)
+#    ns.bind(('127.0.0.1', 7777))
+
+    while True:
+        data, addr=ns.recvfrom(256)
+        datadump = json.loads(data)
+        if datadump['data'] == 'exit':
+            break
+        cipher1 = Caesar(datadump['data'], datadump['key'])
+        print("Plaintext : " +cipher1.plaintext)
+        print("Ciphertext : "+cipher1.cipher())
+        ns.sendto(cipher1.cipher().encode(), addr)
