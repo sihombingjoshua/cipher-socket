06c1f6e447fe59ce985d8e3d9c4955faf23878ab
sihombingjoshua <joshuasihombing1joshua@gmail.com>
2024-11-08 11:52:53 +0700

    better docum dengan comment

--- client cipher.py
+++ client cipher.py
@@ -1,19 +1,19 @@
-import socket
+import socket       # memanggil modul
 import json
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:
-    a = {'data' : 'PoltekSSN', 'key' : 3}
-    ajson = json.dumps(a)
-    ns.sendto(ajson.encode(), ('127.0.0.1', 7777))
-    data, addr=ns.recvfrom(256)
-    print(data.decode())
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:    # memanggil socket sebagai ns
+    a = {'data' : 'PoltekSSN', 'key' : 3}   # data pertama yang dikirimkan
+    ajson = json.dumps(a)   # merubah dalam bentuk json
+    ns.sendto(ajson.encode(), ('127.0.0.1', 7777))  # mengirim kepada server
+    data, addr=ns.recvfrom(256) # menunggu response server
+    print("ciphertext:", data.decode())    # mencetak response server
 
-    shift = int(input("shift?: "))
+    shift = int(input("shift?: ")) # meminta key shift terbaru
 
     i = 1
-    while True: 
+    while True: # memulai program interaktif
         b = {}
-        plaintxt = input("["+str(i)+"]plaintxt: ")
-        if plaintxt == 'exit':
+        plaintxt = input("["+str(i)+"]plaintxt: ")  # meminta inputan plaintext
+        if plaintxt == 'exit':  # program akan berhenti jika inputan = "exit"
             b['data'] = plaintxt
             b['key'] = shift
             ajson = json.dumps(b)
@@ -21,10 +21,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:
             break
         b['data'] = plaintxt
         b['key'] = shift
-        ajson = json.dumps(b)
-        ns.sendto(ajson.encode(), ('127.0.0.1', 7777))
-        data, addr=ns.recvfrom(256)
-        print("["+str(i)+"]ciphertext:", data.decode())
+        ajson = json.dumps(b)   # merubah dalam bentuk json
+        ns.sendto(ajson.encode(), ('127.0.0.1', 7777))  # mengirimkan ke server
+        data, addr=ns.recvfrom(256) # mendapatkan response
+        print("["+str(i)+"]ciphertext:", data.decode()) # mencetak hasil pada layar
         i += 1
 
 
--- server cipher.py
+++ server cipher.py
@@ -1,13 +1,13 @@
-import socket
+import socket   # memanggil modul
 import json
 
-class Caesar:
-    def __init__(self, plain, shift):
-        self.plaintext = plain
+class Caesar:   # membuat kelas Caesar, untuk mengenkripsi
+    def __init__(self, plain, shift):  
+        self.plaintext = plain # membentuk variabel
         self.shift = shift
-    def cipher(self):
+    def cipher(self):   # menambahkan method untuk mengenkripsi
         results = ''
-        for char in self.plaintext:
+        for char in self.plaintext: # proses enkripsi
             if char.isupper():
                 results += chr((ord(char) + self.shift - 65) % 26 + 65)
             elif char.islower():
@@ -15,25 +15,25 @@ class Caesar:
             else:
                 results += char
         self.ciphertext = results
-        return self.ciphertext
+        return self.ciphertext # mengembalikan hasil enkripsi
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns:
-    ns.bind(('127.0.0.1', 7777))
-    data, addr=ns.recvfrom(256)
-    datadump = json.loads(data)
-    cipher1 = Caesar(datadump['data'], datadump['key'])
-    print("Plaintext : " +cipher1.plaintext)
-    print("Ciphertext : "+cipher1.cipher())
-    ns.sendto(cipher1.cipher().encode(), addr)
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as ns: # memanggil modul socket sebagai ns
+    ns.bind(('127.0.0.1', 7777))    # membind port
+    data, addr=ns.recvfrom(256) # menunggu request pertama
+    datadump = json.loads(data) # merubah request pertama dari json menjadi dictionary
+    cipher1 = Caesar(datadump['data'], datadump['key']) # mengenkripsi data dengan membuat object
+    print("Plaintext : " +cipher1.plaintext) # mencetak request pertama yaitu PoltekSSN
+    print("Ciphertext : "+cipher1.cipher()) # mencetak request pertama yaitu SrowhnVVQ
+    ns.sendto(cipher1.cipher().encode(), addr) # mengirimkan hasil enkripsi kepada client
 
     i = 1
-    while True:
-        data, addr=ns.recvfrom(256)
-        datadump = json.loads(data)
-        if datadump['data'] == 'exit':
+    while True: # memulai program interaktif
+        data, addr=ns.recvfrom(256) # menunggu request
+        datadump = json.loads(data) # mengonversi request dari json menjadi dictionary
+        if datadump['data'] == 'exit': # jika request = exit, maka program ditutup
             break
-        cipher1 = Caesar(datadump['data'], datadump['key'])
-        print("[",i,"]Plaintext : " +cipher1.plaintext)
-        print("[",i,"]Ciphertext : "+cipher1.cipher())
-        ns.sendto(cipher1.cipher().encode(), addr)
+        cipher1 = Caesar(datadump['data'], datadump['key']) # mengenkripsi data dengan membuat object
+        print("[",i,"]Plaintext : " +cipher1.plaintext) # mencetak plaintext
+        print("[",i,"]Ciphertext : "+cipher1.cipher()) # mencetak hasil ciphertext
+        ns.sendto(cipher1.cipher().encode(), addr) # mengirimkan hasil enkripsi kepada client
         i += 1
